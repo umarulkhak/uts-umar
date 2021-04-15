390aa8d410b1100ade24b1706dd01d47e5c154f4
umarulkhak <bintang.24khak@gmail.com>
2021-04-15 22:24:13 +0700

    indexing

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('list-kendaraan');
 });
 Route::get('/list-kendaraan', [KendaraanController::class, 'home']);
 Route::get('/form-tambah', function () {
