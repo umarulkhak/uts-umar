c06392a675cc1e137077ee42a605799ebbe49329
umarulkhak <bintang.24khak@gmail.com>
2021-04-15 22:55:54 +0700

    indexing 6

--- resources/views/list-kendaraan.blade.php
+++ resources/views/list-kendaraan.blade.php
@@ -59,7 +59,7 @@
   </div>
 
   <nav class="navbar fixed-bottom navbar-light bg-light">
-    <a class="navbar-brand" href="#">Fixed bottom</a>
+    <a class="navbar-brand">Created by Umar Ulkhak || 2021</a>
   </nav>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
